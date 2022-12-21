248c54494a9a387b680f14f7855cd01f042c1bc6
Akshyay-Dev <64956391+Akshyay-Dev@users.noreply.github.com>
2022-12-21 08:38:11 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    var url = 'mongodb://localhost:27017';
+    var url = 'mongodb+srv://AkshayDev:Mmsa123@cluster0.txolaq9.mongodb.net/?retryWrites=true&w=majority';
     const dbname='shopping'
     MongoClient.connect(url, (err, data)=>{
         if(err) return done(err)
